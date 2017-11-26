dd452754816fa54c8e03eb76ae5c533c3e34a8de
antonio0023 <antonio.2310cv@gmail.com>
2017-11-26 15:36:04 -0600

    modificado icono

--- src/Interfaces/MenuCostos.java
+++ src/Interfaces/MenuCostos.java
@@ -39,9 +39,11 @@ public class MenuCostos extends javax.swing.JFrame {
         jLabel3 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
         jLabel4 = new javax.swing.JLabel();
-        lblAtras = new javax.swing.JLabel();
-        btnAtras = new javax.swing.JButton();
-        jLabel1 = new javax.swing.JLabel();
+        lblAtras1 = new javax.swing.JLabel();
+        btnAtras1 = new javax.swing.JButton();
+        jLabel5 = new javax.swing.JLabel();
+        btnPlanillas = new javax.swing.JButton();
+        bgCostos = new javax.swing.JLabel();
 
         jLabel3.setFont(new java.awt.Font("Arial Black", 0, 14)); // NOI18N
         jLabel3.setForeground(new java.awt.Color(255, 255, 255));
@@ -61,37 +63,52 @@ public class MenuCostos extends javax.swing.JFrame {
         jLabel4.setText("Menu COSTOS");
         getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 10, -1, -1));
 
-        lblAtras.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
-        lblAtras.setForeground(new java.awt.Color(255, 255, 255));
-        lblAtras.setText("Atras");
-        getContentPane().add(lblAtras, new org.netbeans.lib.awtextra.AbsoluteConstraints(540, 320, -1, -1));
+        lblAtras1.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        lblAtras1.setForeground(new java.awt.Color(255, 255, 255));
+        lblAtras1.setText("Atras");
+        getContentPane().add(lblAtras1, new org.netbeans.lib.awtextra.AbsoluteConstraints(540, 380, -1, -1));
 
-        btnAtras.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/salir.png"))); // NOI18N
-        btnAtras.addActionListener(new java.awt.event.ActionListener() {
+        btnAtras1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/salir.png"))); // NOI18N
+        btnAtras1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnAtrasActionPerformed(evt);
+                btnAtras1ActionPerformed(evt);
             }
         });
-        getContentPane().add(btnAtras, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 340, 47, -1));
+        getContentPane().add(btnAtras1, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 320, 47, 60));
 
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/cafe.png"))); // NOI18N
-        jLabel1.setText("jLabel1");
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 610, 420));
+        jLabel5.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        jLabel5.setText("Planillas");
+        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 110, -1, -1));
+
+        btnPlanillas.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/contabilidad.png"))); // NOI18N
+        btnPlanillas.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnPlanillasActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btnPlanillas, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 130, -1, -1));
+
+        bgCostos.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/cafe.png"))); // NOI18N
+        bgCostos.setText("jLabel1");
+        getContentPane().add(bgCostos, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 600, 420));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        IniciarSesion Ini = new IniciarSesion();
-        Ini.setVisible(true);
-        this.setVisible(false);
+
     }//GEN-LAST:event_jButton1ActionPerformed
 
-    private void btnAtrasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAtrasActionPerformed
+    private void btnPlanillasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPlanillasActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnPlanillasActionPerformed
+
+    private void btnAtras1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAtras1ActionPerformed
+        // TODO add your handling code here:
         MenuConta menu = new MenuConta();
         menu.setVisible(true);
         this.setVisible(false);
-    }//GEN-LAST:event_btnAtrasActionPerformed
+    }//GEN-LAST:event_btnAtras1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -124,17 +141,19 @@ public class MenuCostos extends javax.swing.JFrame {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new MenuCostos().setVisible(true);
-                                
+
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnAtras;
+    private javax.swing.JLabel bgCostos;
+    private javax.swing.JButton btnAtras1;
+    private javax.swing.JButton btnPlanillas;
     private javax.swing.JButton jButton1;
-    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel lblAtras;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel lblAtras1;
     // End of variables declaration//GEN-END:variables
 }
